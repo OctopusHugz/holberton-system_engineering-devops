2c9825a098145bf3017f819ea892d46e12dd98b6
OctopusHugz <colsonscott53@gmail.com>
2020-06-23 04:52:04 +0000

    Added first attempt at exercise 11

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,3 +1,7 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - this function creates 5 zombie processes
  * Return: 0
@@ -5,5 +9,16 @@
 
 int main(void)
 {
+	int i = 0;
+	pid_t child_pid;
+
+	while (i < 5)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
+			return (0);
+		printf("Zombie process created, PID: %d\n", child_pid);
+		i++;
+	}
 	return (0);
 }
