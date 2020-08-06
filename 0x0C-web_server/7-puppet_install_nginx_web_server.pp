a9a6634e36b70a9dcd12758f1f1f434ebeadb019
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 17:46:41 +0000

    Added another \t before return

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # This manifest configures a server specifically
-$line = "\tlocation /redirect_me {\n\treturn 301 https://www.youtube.com/watch?v=F0ddL6VaAVs;\n\t}"
+$line = "\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=F0ddL6VaAVs;\n\t}"
 package { 'nginx':
   ensure   => 'installed',
   provider => 'apt'
