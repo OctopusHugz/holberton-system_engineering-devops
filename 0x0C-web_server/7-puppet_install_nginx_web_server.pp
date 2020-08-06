cc3f825446076d74bd1090d512bc02a87990995d
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 17:30:23 +0000

    Added line variable

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,5 @@
 # This manifest configures a server specifically
+$line = "\tlocation /redirect_me {\n\treturn 301 https://www.youtube.com/watch?v=F0ddL6VaAVs;\n\t}"
 package { 'nginx':
   ensure   => 'installed',
   provider => 'apt'
@@ -16,8 +17,6 @@ file { 'index.html':
 
 file_line { '301 redirection':
   path  => '/etc/nginx/sites-available/default',
-  line  => '    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=F0ddL6VaAVs;
-        }',
+  line  => $line,
   after => 'server_name _;'
 }
