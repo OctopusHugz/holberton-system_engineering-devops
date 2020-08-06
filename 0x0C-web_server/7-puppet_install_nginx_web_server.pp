aed43aeb619a4e8ac0e290044835b9380bcd217c
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 03:47:33 +0000

    Added nginx restart at the end

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,7 +5,8 @@ package { 'nginx':
 }
 
 service { 'nginx':
-  ensure  => 'running'
+  ensure  => 'running',
+  restart => ''
 }
 
 file { 'index.html':
@@ -18,8 +19,4 @@ file_line { '301 redirection':
   path  => '/etc/nginx/sites-available/default',
   line  => '\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   after => '^root /var/www/html;$'
-}
-
-service { 'nginx':
-  restart => ''
 }
