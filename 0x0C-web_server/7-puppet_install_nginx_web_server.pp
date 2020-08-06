b3ed41d4c151fc4224bdba709d7dd1d1bb0dbfb3
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 20:29:56 +0000

    Added /usr/bin/env in front of sed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # This manifest configures a server specifically
 #$line = "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
-$command = "sed -i '36a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default"
+$command = "/usr/bin/env sed -i '36a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default"
 
 package { 'nginx':
   ensure   => 'installed',
