5882072e42a62001ea61935003dce9dc886c8f2a
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 04:26:42 +0000

    Fixed regex on after attribute

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -17,5 +17,5 @@ file { 'index.html':
 file_line { '301 redirection':
   path  => '/etc/nginx/sites-available/default',
   line  => '    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  after => 'root /var/www/html;'
+  after => '/var/www/html;'
 }
