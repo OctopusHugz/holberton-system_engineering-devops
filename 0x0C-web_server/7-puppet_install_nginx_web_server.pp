305e80857836c442e14d2be52587e50d1f6a6470
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 19:11:57 +0000

    Moved ensure running to end

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,8 @@
 # This manifest configures a server specifically
-include stdlib
 $line = "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
 package { 'nginx':
   ensure   => 'installed',
   provider => 'apt'
-}
-
-service { 'nginx':
-  ensure  => 'running'
 }
 
 file { 'index.html':
@@ -20,4 +15,8 @@ file_line { '301 redirection':
   path  => '/etc/nginx/sites-available/default',
   line  => $line,
   after => '/var/www/html;'
+}
+
+service { 'nginx':
+  ensure  => 'running'
 }
