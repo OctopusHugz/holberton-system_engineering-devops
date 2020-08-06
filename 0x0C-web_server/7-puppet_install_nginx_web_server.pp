3ac1792b68831bcddf76afb64b20809fbb781b1f
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 19:34:28 +0000

    Changed stdlib name to puppetlabs-stdlib

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,7 @@
 # This manifest configures a server specifically
 $line = "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
 
-package { 'stdlib':
+package { 'puppetlabs-stdlib':
   ensure => 'installed',
   source => 'https://forge.puppet.com/v3/files/puppetlabs-stdlib-6.3.0.tar.gz'
 }
