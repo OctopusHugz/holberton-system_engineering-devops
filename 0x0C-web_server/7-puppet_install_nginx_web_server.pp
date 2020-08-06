907fd57f85cf417c7c59c8ecdc92fe55ea275609
OctopusHugz <colsonscott53@gmail.com>
2020-08-06 03:24:37 +0000

    Added changes to ensure

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,10 +1,10 @@
 # This manifest configures a server specifically
 package { 'nginx':
-  ensure => 'present'
+  ensure => 'installed'
 }
 
 file { 'index.html':
-  ensure  => 'present',
+  ensure  => file,
   path    => '/var/www/html/index.html',
   content => 'Holberton School is cool'
 }
@@ -12,5 +12,5 @@ file { 'index.html':
 file_line { '301 redirection':
   path  => '/etc/nginx/sites-available/default',
   line  => '\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  after => 'var/www/html;'
+  after => 'root /var/www/html;'
 }
