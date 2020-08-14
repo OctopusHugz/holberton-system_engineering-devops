48fc9d8605effbd2d61c651e2b67b614416833f3
OctopusHugz <colsonscott53@gmail.com>
2020-08-14 23:47:18 +0000

    Added chains to ensure order

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,29 +1,29 @@
 # This manifest configures a server specifically
-$update = "/usr/bin/env apt-get -y update"
-$command = "/usr/bin/env sed -i '14a add_header X-Served-By $hostname;' /etc/nginx/nginx.conf"
+$update = '/usr/bin/env apt-get -y update'
+$command = "/usr/bin/env sed -i '14a add_header X-Served-By ${hostname};' /etc/nginx/nginx.conf"
 
 exec { 'apt-get update':
   command => $update,
   before  => Package['nginx']
 }
 
-package { 'nginx':
+-> package { 'nginx':
   ensure   => 'installed',
   provider => 'apt'
 }
 
-file { 'index.html':
-  ensure  => file,
-  path    => '/var/www/html/index.html',
-  content => 'Holberton School is cool'
-}
-
-exec { 'custom HTTP header':
+-> exec { 'custom HTTP header':
   command => $command,
   require => Package['nginx']
 }
 
-service { 'nginx':
+-> file { 'index.html':
+  ensure  => file,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School is cool',
+  require => Package['nginx']
+}
+-> service { 'nginx':
   ensure  => 'running',
   restart => 'sudo service nginx restart'
 }
