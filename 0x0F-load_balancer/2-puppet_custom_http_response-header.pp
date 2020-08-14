fb9bde0be08121166cc447967a80271ac90eee3a
OctopusHugz <colsonscott53@gmail.com>
2020-08-14 22:22:19 +0000

    Added solution for exercise 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,22 @@
+# This manifest configures a server specifically
+$command = "/usr/bin/env sed -i '14a \\tadd_header X-Served-By '${HOSTNAME}';' nginx.conf"
+
+package { 'nginx':
+  ensure   => 'installed',
+  provider => 'apt'
+}
+
+file { 'index.html':
+  ensure  => file,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School is cool'
+}
+
+exec { 'custom HTTP header':
+  command => $command
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  restart => 'sudo service nginx restart'
+}
