83516fd6070184297202a542e6cb198c0c6a5a2d
OctopusHugz <colsonscott53@gmail.com>
2020-09-14 19:31:35 +0000

    Refactored logic for 0 to be cleaner. Added solution for exercise 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,20 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
-    if argv[1]:
-        emp_id = argv[1]
-        user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-            emp_id)
-        emp_name = requests.get(user_url).json().get('name')
-        td_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            emp_id)
-        td_request_json = requests.get(td_url).json()
-        td_length = len(td_request_json)
-        td_done = 0
-        task_list = []
-        for todos in td_request_json:
-            if todos.get('completed') is True:
-                td_done += 1
-                task_list.append(todos.get('title'))
-        print("Employee {} is done with tasks({:d}/{:d}):".format(emp_name,
-                                                                  td_done,
-                                                                  td_length))
-        [print("\t{}".format(task)) for task in task_list]
+    emp_id = argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_url = "{}users/{}".format(base_url, emp_id)
+    emp_name = requests.get(user_url).json().get('name')
+    td_url = "{}/todos".format(user_url)
+    td_request_json = requests.get(td_url).json()
+    td_length = len(td_request_json)
+    td_done = 0
+    task_list = []
+    for todos in td_request_json:
+        if todos.get('completed') is True:
+            td_done += 1
+            task_list.append(todos.get('title'))
+    print("Employee {} is done with tasks({:d}/{:d}):".format(emp_name,
+                                                                td_done,
+                                                                td_length))
+    [print("\t {}".format(task)) for task in task_list]
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module gathers data from an API and exports it to a CSV file"""
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+    emp_id = argv[1]
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_url = "{}users/{}".format(base_url, emp_id)
+    emp_uname = requests.get(user_url).json().get('username')
+    td_url = "{}/todos".format(user_url)
+    td_request_json = requests.get(td_url).json()
+    td_length = len(td_request_json)
+    td_done = 0
+    task_list = []
+    filename = emp_id + ".csv"
+    with open(filename, "w") as f:
+        td_writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
+        for td in td_request_json:
+            td_writer.writerow([td['userId'], emp_uname, td['completed'],
+                                td['title']])
