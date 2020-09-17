b21b71960da0e25b276114a7efa5216dd3c97b21
OctopusHugz <colsonscott53@gmail.com>
2020-09-17 18:46:35 +0000

    Added allow_redirects=False to request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     subs = 0
-    user_agent = {'User-agent': 'Safari/605.1.15'}
     req = requests.get(
         "https://www.reddit.com/r/{}/about/.json".format(subreddit),
-        headers=user_agent)
-    data = req.json().get('data')
-    subs = data.get('subscribers')
+        allow_redirects=False)
+    data = req.json().get('data').get('subscribers')
     if subs is None:
         subs = 0
     return subs
