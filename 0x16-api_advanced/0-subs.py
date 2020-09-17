a38b35662d80d05bd492aee1eb7cf08847a5624e
OctopusHugz <colsonscott53@gmail.com>
2020-09-17 18:51:58 +0000

    Trying this again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
+    user_agent = {'User-agent': 'Safari/605.1.15'}
     req = requests.get(
         "https://www.reddit.com/r/{}/about/.json".format(subreddit),
-        allow_redirects=False)
-    subs = req.json().get('data').get('subscribers')
+        headers=user_agent, allow_redirects=False)
+    data = req.json().get('data')
+    subs = data.get('subscribers')
     if subs is None:
         subs = 0
     return subs
