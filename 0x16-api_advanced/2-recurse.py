9b629c2ade387d76efcf990325c22badbe76350d
OctopusHugz <colsonscott53@gmail.com>
2020-09-17 23:29:46 +0000

    Refactored recursive solution

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,34 +6,24 @@ import requests
 def recurse(subreddit, hot_list=[], count=0, after=""):
     """Prints the titles of the first 10 hot posts listed for a subreddit"""
     user_agent = {'User-agent': 'Safari/605.1.15'}
-    # This should be 1st request only, subsequent should add after=t3_...
     if not after:
         req = requests.get(
             "https://www.reddit.com/r/{}/hot/.json?limit=100".format(
                 subreddit),
             headers=user_agent, allow_redirects=False)
-        data = req.json().get('data')
-        if data is None:
-            return None
-        after = data.get('after')
-        children = data.get('children')
-        for child in children:
-            hot_list.append(child.get('data').get('title'))
-        count += len(children)
-        recurse(subreddit, hot_list, count, after)
     else:
         req = requests.get(
             "https://www.reddit.com/r/{}/hot/.json?limit=100&after={}".format(
                 subreddit, after),
             headers=user_agent, allow_redirects=False)
-        data = req.json().get('data')
-        if data is None:
-            return None
-        after = data.get('after')
-        children = data.get('children')
-        for child in children:
-            hot_list.append(child.get('data').get('title'))
-        count += len(children)
-        if after is not None:
-            recurse(subreddit, hot_list, count, after)
+    data = req.json().get('data')
+    if data is None:
+        return None
+    after = data.get('after')
+    children = data.get('children')
+    for child in children:
+        hot_list.append(child.get('data').get('title'))
+    count += len(children)
+    if after is not None:
+        recurse(subreddit, hot_list, count, after)
     return hot_list
