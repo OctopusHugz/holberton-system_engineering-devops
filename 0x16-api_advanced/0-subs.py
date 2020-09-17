ef1c08b65a76f6e927a233ebb4df5db589b5134f
OctopusHugz <colsonscott53@gmail.com>
2020-09-17 18:36:39 +0000

    Added solution for exercise 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module queries the Reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    user_agent = {'User-agent': 'OctopusHugs/1.0'}
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about/.json".format(subreddit),
+        headers=user_agent)
+    data = req.json().get('data')
+    subs = data.get('subscribers')
+    if subs is None:
+        subs = 0
+    return subs
