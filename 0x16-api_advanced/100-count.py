44f5772a8f646bf84eab44b7bd2835fab6a69d76
OctopusHugz <colsonscott53@gmail.com>
2020-09-23 17:32:15 +0000

    Added updated solution

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,7 +5,7 @@ import requests
 
 def count_words(subreddit, word_list, count=0, after="", hot_list=[], title_dict={}, word_count=0):
     """Prints a sorted count of given keywords listed for a subreddit"""
-    user_agent = {'User-agent': 'Safari/605.1.15'}
+    user_agent = {'User-agent': 'OctopusHugs/605.1.15'}
     if not after:
         req = requests.get(
             "https://www.reddit.com/r/{}/hot/.json?limit=100".format(
@@ -25,28 +25,31 @@ def count_words(subreddit, word_list, count=0, after="", hot_list=[], title_dict
         title = child.get('data').get('title').lower().split()
         for word in word_list:
             word = word.lower()
+            word_count = title_dict.get(word)
+            if word_count is None:
+                word_count = 0
             word_count += title.count(word)
-            if word_count > 0:
+            if word_count > 0 and word in title:
                 title_dict.update({word: word_count})
-            # print(title_dict)
-        # for word in word_list:
-        #     word = word.lower()
-        #     if word in title:
-        #         hot_list.append(title)
-        #         word_count = title_dict.get(word)
-        #         if word_count is None:
-        #             word_count = 1
-        #         else:
-        #             word_count += 1
-        #         title_dict.update({word: word_count})
     count += len(children)
     if after is not None:
         count_words(subreddit, word_list, count, after,
                     hot_list, title_dict, word_count)
     else:
-        # dict_copy = title_dict.copy()
-        for keyword in title_dict:
-            # max_title = ""
-            print("{}: {:d}".format(keyword, title_dict.get(keyword)))
-    # print(hot_list)
+        values = title_dict.values()
+        values = list(values)
+        # for value in values:
+        #     max_value = max(values)
+        #     if value == max_value:
+        #         print("{}: {:d}".format(key, title_dict.get(key)))
+        while len(values) > 0:
+            for key, value in title_dict.items():
+                if len(values) > 1:
+                    max_value = max(values)
+                elif len(values) == 1:
+                    max_value = values[0]
+                if value == max_value:
+                    print("{}: {:d}".format(key, title_dict.get(key)))
+                    index = values.index(max_value)
+                    del values[index]
     return hot_list
