e5e50d04ba9a887c47a06bcb4ec7f4f2d7a7cfad
OctopusHugz <colsonscott53@gmail.com>
2020-10-06 05:19:43 +0000

    Added puppet script for exercise 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# This manifest configures a server specifically
+$command = "/usr/bin/env sed -i 's/15/4096/g' /etc/default/nginx && /usr/sbin/service nginx restart"
+
+exec { 'Fix nginx worker limit':
+  command => $command
+}
